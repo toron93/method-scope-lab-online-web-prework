373f509a58ae011a1ade30377c216acc5b52f057
Toron Seigler <toronseigler9@gmail.com>
2019-06-11 17:55:06 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
-describe "#catch_phrase" do 
-  it "puts out a catch phrase" do 
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
     phrase = "It's-a me, Mario!"
     expect{catch_(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
